5bd02fa75e5580a9a2ed47d54199fa16c81655f2
leah4912 <leah29726@gmail.com>
2020-10-03 16:59:20 -0400

    added aws cli - connecting to dynamo db next

--- route-notes.js
+++ route-notes.js
@@ -86,3 +86,12 @@
 //   const randomQuote = await records.getRandomQuote();
 //   res.json(randomQuote);
 // });
+
+
+// aws dynamodb create-table \
+//     --table-name Music \
+//     --attribute-definitions \
+//         AttributeName=Artist,AttributeType=S \
+//         AttributeName=SongTitle,AttributeType=S \
+//     --key-schema AttributeName=Artist,KeyType=HASH AttributeName=SongTitle,KeyType=RANGE \
+//     --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1
